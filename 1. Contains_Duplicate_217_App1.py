c91f9280e8b49bbfb738eb6339adb0eef59de0e0
Vipin19174 <168709143+Vipin19174@users.noreply.github.com>
2024-09-21 22:42:07 +0530

    1. Contains_Duplicate_217_App1
    1. Contains_Duplicate_217_App1

---
+++ 1. Contains_Duplicate_217_App1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        
+        nums.sort()
+        f=0
+
+        for i in range(len(nums)-1):
+            if nums[i]==nums[i+1]:
+                return True
+        
+        return False
